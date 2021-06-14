3ca3e6274869fdc0253e2389d86bdc4faa73e9b7
Nacho <nachomigoni@gmail.com>
2021-06-14 01:29:42 -0300

    clase 21 casi lista

--- Práctica 3/Clase 21/index.js
+++ Práctica 3/Clase 21/index.js
@@ -2,15 +2,30 @@ const articulos = {
 
     datos(){
         return {
+            titulo:'',
+            url:'',
             lista_articulos:[]
         }
 
     },
 
     methods: {
+        guardar_articulo(){
+            let articulo = {
+                titulo:this.titulo,
+                url:this.url
+            }
 
+            this.lista_articulos.push(this.guardar_articulo)
+
+            this.titulo='', this.url=''
+        }
+        
+    },
+    mounted(){
         
     }
 
 
 }
+Vue.createApp(articulos).mount("#art")
