841055ad5c61b7ff81d03c46c50268e0eb35efae
Nacho <nachomigoni@gmail.com>
2021-10-01 19:24:52 -0300

    Proyecto 2 y aplicación de Producto terminada

---
+++ Práctica 3/Vue/proyecto_1/src/views/Producto.vue
@@ -0,0 +1,13 @@
+<template>
+    <ProductoComp></ProductoComp>
+</template>
+
+<script>
+import ProductoComp from '@/components/ProductoComp.vue';
+export default {
+    name:'Producto',
+    components:{
+        ProductoComp
+    }
+}
+</script>
