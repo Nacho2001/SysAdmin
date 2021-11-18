c5a726af68cea21f752498d615006cd8e1e996d5
Nacho <nachomigoni@gmail.com>
2021-11-18 13:36:24 -0300

    Server monitoreo listo

---
+++ Práctica 3/Práctico_Final/server-monitoreo/src/database.js
@@ -0,0 +1,24 @@
+//Invoco el servicio mysql
+const mysql = require('mysql')
+
+//Ingreso a la BD
+const db = mysql.createConnection({
+    host:'192.168.56.102',
+    port:3306,
+    //server:'db',
+    user:'root',
+    password:'migoni2001',
+    database:'monitoreo_db'
+});
+
+//conexión en marcha
+db.connect(function(err){
+    if(err){
+        console.log(err)
+        return;
+    }else{
+        console.log("Conexión con la BD exitosa!!")
+    }
+})
+
+module.exports = db
