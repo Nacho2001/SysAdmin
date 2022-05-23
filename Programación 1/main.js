7fb90573068e70a691d7b3de279ea3c14fa86e86
Nacho <nachomigoni@gmail.com>
2022-05-23 20:23:41 -0300

    Clase 23/05/2022

--- Programación 1/main.js
+++ Programación 1/main.js
@@ -1,1 +1,48 @@
-jss
+/* let edad = parseInt(prompt("Ingrese la edad"));
+
+let letra = ""
+if (edad >= 18){
+    letra = "numero"
+    document.write("La persona de " + edad + "años es mayor a 18");
+}else{
+    document.write("Es menor de edad")
+}
+
+switch (numero) {
+    case "+":
+        break;
+    case "-":
+        break;
+    default:
+        break;
+
+}
+
+//Bucles
+
+while (condicion) {
+    //comandos
+}
+do {
+
+} while (condicion);
+for (let index = 0; index < Array.length; index++) {
+    const element = array[index];
+} */
+do {
+    condicion = confirm("quiere seguir?");
+    alert("usted ha pulsado " + condicion)
+} while (condicion)
+/*confirm()
+while(condicion){
+
+}
+var arreglo = [1.2,3,7,12,true,"n"]
+arreglo[1]=5
+for (let item of arreglo) {
+    document.write(item+"<br>");
+}
+
+for (let i = 0; i < arreglo.length; i++){
+    document.write(arreglo[i]+"<br>");
+}*/
